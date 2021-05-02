a8678c9b57d211735e5a722ed43ac03bf77ae091
dwl5 <ssop6403@gmail.com>
2021-05-02 19:01:43 +0900

    feat: logger

--- src/main/java/chess/controller/ControllerExceptionHandler.java
+++ src/main/java/chess/controller/ControllerExceptionHandler.java
@@ -1,6 +1,9 @@
 package chess.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,13 +11,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> checkChessGameException(Exception exception) {
+        log.error("checkChessGameException called. exception : " + exception.getMessage());
+        fileLogger.info("파일 로깅 입니다.");
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
     @ExceptionHandler({MissingRequestCookieException.class})
     public ResponseEntity<String> checkCookieMissing(Exception exception) {
+        log.error("checkChessGameException called. exception : " + exception.getMessage());
+        fileLogger.info("파일 로깅 입니다.");
         return ResponseEntity.badRequest().body("로그인을 해 주세요.");
     }
 
